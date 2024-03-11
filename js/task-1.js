0fe5185edba3f4279dc09f1117fbd0245c4e2cad
Vitaly112 <vitalyishchenko@gmail.com>
2024-03-11 10:20:34 +0200

    1

--- js/task-1.js
+++ js/task-1.js
@@ -1,7 +1,9 @@
-function makeTransaction (quantity, pricePerDroid) {
-    const sum = quantity * pricePerDroid;
-return `You ordered ${quantity} droids worth ${sum} credits!`;
-}
-console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
-console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
-console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
+const categoriesTotal = document.querySelector("#categories");
+const categoriesCount = categoriesTotal.children.length;
+console.log(`Number of categories: ${categoriesCount}`);
+
+const listByClass = document.querySelectorAll(".item");
+listByClass.forEach((item) => {
+  console.log("Category: ", item.firstElementChild.textContent); 
+  console.log("Elements: ", item.lastElementChild.children.length);
+});
